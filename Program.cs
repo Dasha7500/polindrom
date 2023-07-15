08fd4a6f559cddedeab67de4f66fb9e80665b63d
daryna.fesenko <darina071602@gmail.com>
2023-07-15 08:18:40 +0200

    poli

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+        Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPalindrome = IsPalindrome(number);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Число является палиндромом.");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом.");
+        }
+
+    static bool IsPalindrome(int number)
+    {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
